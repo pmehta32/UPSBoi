f143e5c5e589d94e6e67412380b7b85f9863cc0d
rajatcodesx <67538489+rajat698@users.noreply.github.com>
2022-10-07 18:08:46 -0400

    JFile chooser in UI

--- src/GUI.java
+++ src/GUI.java
@@ -1,15 +1,56 @@
 import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.ButtonModel;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JFileChooser;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+import javax.swing.border.Border;
 
-public class GUI {
+public class GUI implements ActionListener{
+
+    JButton button;
 
     public GUI() {
 
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
+        JButton button = new JButton("Upload file");
+        button.addActionListener(this);
+
+
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 10));
-        // panel.setLayout(new GridLayout(0, 1));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Travelling Salesman");
+        frame.pack();
+        frame.setVisible(true);
 
     }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        if(e.getSource() == button) {
+
+            JFileChooser fileUploader = new JFileChooser();
+            fileUploader.setVisible(true);
+            fileUploader.showOpenDialog(null);
+        }
+    }
+
     public static void main(String[] args) {
 
         new GUI();

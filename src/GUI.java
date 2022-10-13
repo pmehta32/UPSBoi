398f897c047cdf588d760999ace95ed811ca94fc
kartavya-asu <90439756+kartavya-asu@users.noreply.github.com>
2022-10-12 19:55:00 -0700

    GUI Update V-3

--- src/GUI.java
+++ src/GUI.java
@@ -44,7 +44,7 @@ public class GUI extends JFrame implements ActionListener{
 //        this.removeAll();
         setTitle("TSP");
         setBounds(300, 90, 900, 800   );
-        //setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
 
         Container container = getContentPane();
@@ -52,7 +52,7 @@ public class GUI extends JFrame implements ActionListener{
 
         JLabel title = new JLabel("TSP");
         title.setFont(new Font("Arial", Font.PLAIN, 40));
-        title.setSize(300, 40);
+        title.setSize(300, 30);
         title.setLocation(300, 30);
         container.add(title);
 
@@ -60,7 +60,7 @@ public class GUI extends JFrame implements ActionListener{
 
         button = new JButton("Choose File");
         button.setSize(150, 20);
-        button.setLocation(100, 100);
+        button.setLocation(100, 70);
         button.addActionListener(this);
         container.add(button);
 
@@ -68,38 +68,37 @@ public class GUI extends JFrame implements ActionListener{
         fileName = new JLabel("No file selected");
         fileName.setFont(new Font("Arial", Font.PLAIN, 20));
         fileName.setSize(200, 20);
-        fileName.setLocation(400, 100);
+        fileName.setLocation(400, 70);
         container.add(fileName);
 
         JLabel totalDistance = new JLabel("Total Distance:");
         totalDistance.setFont(new Font("Arial", Font.PLAIN, 20));
         totalDistance.setSize(200, 20);
-        totalDistance.setLocation(100, 200);
+        totalDistance.setLocation(100, 120);
         container.add(totalDistance);
 
         calculatedDistance = new JLabel("Not yet calculated");
         calculatedDistance.setFont(new Font("Arial", Font.PLAIN, 20));
         calculatedDistance.setSize(200, 20);
-        calculatedDistance.setLocation(400, 200);
+        calculatedDistance.setLocation(400, 120);
         container.add(calculatedDistance);
 
         JLabel route = new JLabel("Route:");
         route.setFont(new Font("Arial", Font.PLAIN, 20));
         route.setSize(200, 20);
-        route.setLocation(100, 300);
+        route.setLocation(100, 150);
         container.add(route);
 
         textArea = new JTextArea ("Path yet not calculated");
         textArea.setFont(new Font("Arial", Font.PLAIN, 20));
         textArea.setSize(200, 20);
-        textArea.setLocation(400, 300);
+        textArea.setLocation(400, 150);
         JScrollPane scroll = new JScrollPane (textArea,
                 JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scroll.setLocation(400, 300);
-        scroll.setSize(400, 50);
+        scroll.setLocation(400, 150);
+        scroll.setSize(400, 200);
         container.add(scroll);
-//        frame.add(scroll);
-//        frame.setVisible (true);
+
 
 //        JPanel panel2 = new JPanel(){
 ////            @Override
@@ -121,10 +120,16 @@ public class GUI extends JFrame implements ActionListener{
 ////                }
 ////            }
 //        };
+//        panel2.setSize(400, 400);
+        JLabel map = new JLabel("Map:");
+        map.setFont(new Font("Arial", Font.PLAIN, 20));
+        map.setSize(200, 20);
+        map.setLocation(100, 370);
+        container.add(map);
 
         JPanel panel2 = new JPanel();
         panel2.setSize(350, 350);
-        panel2.setLocation(100, 400);
+        panel2.setLocation(400, 370);
 //        panel2.setBackground(Color.black);
         panel2.setBorder(BorderFactory.createLineBorder(Color.black));
         container.add(panel2);

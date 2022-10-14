b68cbf4811c19265f7edf1a19bc9a01e6f89cd81
kartavya-asu <90439756+kartavya-asu@users.noreply.github.com>
2022-10-13 20:15:23 -0700

    code cleaning v-4

--- src/PathDisplay.java
+++ src/PathDisplay.java
@@ -13,8 +13,6 @@ public class PathDisplay extends JTextArea {
     public void showPath(ArrayList path) {
         this.setText("");
         int cnt = 0;
-        //String s = "";
-        //ArrayList s = new ArrayList();
         String row = "";
         for(int i=0; i<path.size(); i++) {
             row += path.get(i).toString();
@@ -25,18 +23,9 @@ public class PathDisplay extends JTextArea {
                 row += "\n";
                 this.append(row);
                 cnt+= row.length();
-                //s += row;
-                //s.add(row);
                 row = "";
             }
         }
         this.append(row);
-        //this.setText(s);
-        //System.out.println(path.subList(0,10));
-        //System.out.println(path.subList(path.size()-10, path.size()));
-        //System.out.println(cnt);
-
-        //System.out.println(s.substring(0, 10));
-        //System.out.println(s.substring(s.length()-10, s.length()));
     }
 }

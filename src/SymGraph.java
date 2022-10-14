1c2dd9c7d9c541eb58fe2c370ad52afc44d7698e
Pankti Mehta <panktimehta.contact@gmail.com>
2022-10-13 21:58:14 -0700

    javadoc for SymGraph.java

--- src/SymGraph.java
+++ src/SymGraph.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
 
+/**
+ * SymGraph class inherits from the Graph class made in the Graph.java
+ * makes the attributes to take the co-ordinate values as a arraylist and save them in a memory
+ * has a constructor which takes vertices and coordinates and stores them in a memory in an optimized way
+ */
 public class SymGraph extends Graph {
-
     private ArrayList<ArrayList> coordinates;
     private boolean memorySaver = false;
+
 
     public SymGraph(int numVertices, ArrayList<ArrayList> coordinates) {
         super(numVertices);
@@ -15,6 +20,11 @@ public class SymGraph extends Graph {
         }
     }
 
+    /**
+     * constructor activates convertCoordsToEdges method
+     * transfers the data of the coordinates to the edges value so that the distance can be calculated
+     * for the symmetric file
+     */
     private void convertCoordsToEdges() {
         ArrayList edges = new ArrayList();
         for(int i=0; i<numVertices; i++) {
@@ -29,16 +39,34 @@ public class SymGraph extends Graph {
         this.setEdges(edges);
     }
 
+    /**
+     * getCoordinates is an Arraylist type method which gets the coordinates from the file
+     * and uses them to calculate distance
+     * overrides the original getCoorsinates method
+     * @return gets the coordinates
+     */
     @Override
     public ArrayList getCoordinates() {
         return this.coordinates;
     }
-
+    /**
+     * isSymmetric method is a Boolean type methos which checks if the graph is displayed for only symmetric file
+     * overrides the original isSymmetric method
+     * @return gives True if symmetric, otherwise False
+     */
     @Override
     public boolean isSymmetric() {
         return true;
     }
 
+    /**
+     * getDistance method has a float dataype because it may have float distance value
+     * calculates the distance between source that is array[0] and destination that is array[1]
+     *
+     * @param source coordinate of the source point of the array
+     * @param destination coordinate of the destination point of the array
+     * @return gives the calculated distance if there is any data in the memory, else just returns the points
+     */
     @Override
     public float getDistance(int source, int destination) {
         if(memorySaver) {

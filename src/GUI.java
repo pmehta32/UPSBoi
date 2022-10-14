39639c7f12cddc355dd4c006dc5a1f083d1f1170
Pankti Mehta <panktimehta.contact@gmail.com>
2022-10-13 19:09:12 -0700

    javadoc for GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -1,66 +1,159 @@
+
+
 import javax.swing.*;
 import javax.swing.BorderFactory;
-import javax.swing.ButtonGroup;
-import javax.swing.ButtonModel;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JFileChooser;
-import javax.swing.SwingConstants;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.awt.*;
-import javax.swing.border.Border;
+import java.util.ArrayList;
 
+/**
+ * public class GUI inherits from a class called JFrame and uses the ActionListener interface
+ * GUI provides the interface for users to uplaod files and check various details
+ * constructor GUI uses to call the method in the class
+ */
 public class GUI extends JFrame implements ActionListener{
 
-    JButton button;
+    JButton chooseFileButton;
+    JLabel fileName;
+    JLabel calculatedDistance;
+    PathDisplay travellingPath;
+    MapPlot mapPanel;
     private Graph graph;
 
-    public GUI() {
 
-        JPanel panel = new JPanel();
-        button = new JButton("Choose File");
-        button.addActionListener(this);
-
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
-        this.setLayout(new GridLayout(0, 1));
-        this.add(button);
-
-        this.add(panel, BorderLayout.CENTER);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Travelling Salesman");
-        this.pack();
-        this.setVisible(true);
-
+    public GUI(){
+        loadGUI();
     }
+
+    /**
+     * loadGUI method handles the entire GUI
+     * handles the default display and the display after input file
+     * when there is no file selected, loadGUI displays the default message
+     * handles the display of the file name with an extension of tsp and atsp
+     * handles the display of the route as a textbox
+     * handles the calculated distance as a textbox
+     * handles the display the graph for the symmetric points
+     * manages all the above functions by creating their attributes
+     */
+    public void loadGUI() {
+        setTitle("Travelling Salesman Problem");
+        setBounds(300, 90, 900, 800   );
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(true);
+
+        Container container = getContentPane();
+        container.setLayout(null);
+
+        JLabel title = new JLabel("TSP");
+        title.setFont(new Font("Arial", Font.PLAIN, 40));
+        title.setSize(300, 30);
+        title.setLocation(300, 30);
+        container.add(title);
+
+        chooseFileButton = new JButton("Choose File");
+        chooseFileButton.setSize(150, 20);
+        chooseFileButton.setLocation(100, 70);
+        chooseFileButton.addActionListener(this);
+        container.add(chooseFileButton);
+
+        fileName = new JLabel("No file selected");
+        fileName.setFont(new Font("Arial", Font.PLAIN, 20));
+        fileName.setSize(200, 20);
+        fileName.setLocation(400, 70);
+        container.add(fileName);
+
+        JLabel totalDistance = new JLabel("Total Distance:");
+        totalDistance.setFont(new Font("Arial", Font.PLAIN, 20));
+        totalDistance.setSize(200, 20);
+        totalDistance.setLocation(100, 120);
+        container.add(totalDistance);
+
+        calculatedDistance = new JLabel("Not yet calculated");
+        calculatedDistance.setFont(new Font("Arial", Font.PLAIN, 20));
+        calculatedDistance.setSize(200, 20);
+        calculatedDistance.setLocation(400, 120);
+        container.add(calculatedDistance);
+
+        JLabel route = new JLabel("Route:");
+        route.setFont(new Font("Arial", Font.PLAIN, 20));
+        route.setSize(200, 20);
+        route.setLocation(100, 150);
+        container.add(route);
+
+        travellingPath = new PathDisplay ("Path yet not calculated");
+        travellingPath.setEnabled(false);
+        travellingPath.setFont(new Font("Arial", Font.PLAIN, 20));
+        travellingPath.setSize(200, 20);
+        travellingPath.setLocation(400, 150);
+        JScrollPane travellingPathScroll = new JScrollPane (travellingPath,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        travellingPathScroll.setLocation(400, 150);
+        travellingPathScroll.setSize(400, 200);
+        container.add(travellingPathScroll);
+
+        JLabel map = new JLabel("Map:");
+        map.setFont(new Font("Arial", Font.PLAIN, 20));
+        map.setSize(200, 20);
+        map.setLocation(100, 370);
+        container.add(map);
+
+        mapPanel = new MapPlot();
+        mapPanel.setSize(350, 350);
+        mapPanel.setLocation(400, 370);
+        mapPanel.setBackground(Color.black);
+        mapPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        container.add(mapPanel);
+
+        this.setVisible(true);
+    }
+
+    /**
+     * actionPerformed method overrides the original method from the ActionListener interface
+     * works as a triggering method.
+     * as the user performs the action, actionPerformed method triggers the in-built functions of the ActionListener
+     *
+     * @param e takes the action from the user as an ActionEvent type object
+     *          and performs the actions defined in the method
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        if(e.getSource() == button) {
-
+        if(e.getSource() == chooseFileButton) {
             JFileChooser fileUploader = new JFileChooser();
             fileUploader.setVisible(true);
             int selectedFile = fileUploader.showOpenDialog(null);
 
             if(selectedFile == JFileChooser.APPROVE_OPTION) {
-                //File file = new File(fileUploader.getSelectedFile().getAbsolutePath());
-                // System.out.println(file);
+                this.graph = null;
                 this.graph = FileReader.loadFile(fileUploader.getSelectedFile().getAbsolutePath());
-                this.graph.printEdges();
+
+                String filepath = fileUploader.getSelectedFile().getName();
+                fileName.setText(filepath);
+
+                PathFinder pf = new PathFinder(this.graph);
+                ArrayList citiesPathDist =  pf.findPath();
+                calculatedDistance.setText(citiesPathDist.get(1).toString());
+
+                travellingPath.showPath((ArrayList)citiesPathDist.get(0));
+
+                if(graph.isSymmetric()) {
+                    mapPanel.setCoordinates((ArrayList) graph.getCoordinates());
+                    mapPanel.repaint();
+                }
             }
         }
     }
 
+    /**
+     * main method is made to create a new object of GUI constructor to call the loadGUI method to perform actions
+     * 
+     * @param args takes the GUI constructor object 
+     */
     public static void main(String[] args) {
-
         new GUI();
-        
     }
 }

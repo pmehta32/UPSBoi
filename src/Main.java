25cda51f1638356e9b8b1ece81f0fa16c59b0344
pmehta32 <119138980+pmehta32@users.noreply.github.com>
2022-12-02 17:23:48 -0800

    Javadoc for Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,11 @@
  */
 public class Main {
     /**
-     * creates a new object of GUI class to trigger the new thread
+     * creates a new object of View class to trigger the new thread
      * @param args takes the runtime arguments for the application of the String type
      */
-    public static void main(String[] args){
-        new GUI();
+
+    public static void main(String args[]) {
+        new View();
     }
 }
